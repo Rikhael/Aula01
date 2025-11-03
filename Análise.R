73ace0fba487f15e5f94eeee407fb903e61c6d1d
Felipe Ceacero <fe_ceacero@id.uff.br>
2025-11-03 10:06:53 -0300

    Incluindo o cálculo da correlação

--- Análise.R
+++ Análise.R
@@ -1,7 +1,11 @@
 #Criando um vetor
-X = c(2,6,8,10)
+X = c(2, 6, 8, 10)
 
 #Calculando a média dos valores
 mean(x=X, na.rm=TRUE)
 
+#Vetor Y
+Y = c(3, 7, 6, 12)
 
+#Calculando correlação entre X e Y
+cor(x=X, y=Y)
